b345fa1d34ba5fdc693f7bb2e36d555b6cc3bd28
yuli hunsicker <yhunsicker@gmail.com>
2019-04-12 17:21:04 -0400

    updated website

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var i=0, text;
+  text = " Hi, I'm Yu-Li! "
+
+function typing() {
+  if(i<text.length){
+  document.getElementById("text").innerHTML += text.charAt(i); i++;
+    setTimeout(typing,150);
+      }
+    }
+typing();
